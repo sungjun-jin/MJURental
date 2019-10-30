f02c79d150a4217816e277fd8c5e9a2aa820f6fd
jinsungjun <dadude23@naver.com>
2019-10-30 16:50:46 +0900

    fragment add

--- app/src/main/java/com/example/projectmjurental/RentalActivity.java
+++ app/src/main/java/com/example/projectmjurental/RentalActivity.java
@@ -2,12 +2,15 @@ package com.example.projectmjurental;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.projectmjurental.data.Battery;
+import com.example.projectmjurental.data.Calculator;
 import com.example.projectmjurental.data.Const;
 import com.example.projectmjurental.data.Notebook;
 import com.example.projectmjurental.fragment.FragmentAdapter;
@@ -15,6 +18,8 @@ import com.example.projectmjurental.fragment.ImageFragment;
 
 import java.util.ArrayList;
 import java.util.Date;
+
+import static com.example.projectmjurental.data.Const.notebook;
 
 public class RentalActivity extends AppCompatActivity {
 
@@ -25,6 +30,8 @@ public class RentalActivity extends AppCompatActivity {
 
     ArrayList<Integer> listImage = new ArrayList<>();
 
+    FragmentAdapter fragmentAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +40,57 @@ public class RentalActivity extends AppCompatActivity {
 
         init();
 
+        //test용
 //        rentalObject = "명지대학교노트북";
+//        rentalObject = "명지대학교배터리";
+//        rentalObject = "명지대학교계산기";
+
         getRentalObject();
 
-        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager());
+        setImage();
+    }
+
+    void setImage() {
+
+//        Fragment에 대여 이미지 삽입
+
+        fragmentAdapter = new FragmentAdapter(getSupportFragmentManager());
 
         //ViewPager와 FragmentAdapter 연결
 
         viewPager.setAdapter(fragmentAdapter);
 
-        listImage.add(R.drawable.gram1);
-        listImage.add(R.drawable.gram2);
-        listImage.add(R.drawable.gram3);
+        if (rentalObject.equals(notebook)) {
 
-        for(int i=0;i<3;i++) {
+            Log.i("DEBUG_CODE","노트북");
+
+            listImage.add(R.drawable.gram1);
+            listImage.add(R.drawable.gram2);
+            listImage.add(R.drawable.gram3);
+
+        } else if (rentalObject.equals(Const.battery)) {
+
+            Log.i("DEBUG_CODE","배터리");
+
+            //보조배터리
+
+            listImage.add(R.drawable.battery1);
+            listImage.add(R.drawable.battery2);
+            listImage.add(R.drawable.battery3);
+
+
+        } else if (rentalObject.equals(Const.calculator)) {
+
+            Log.i("DEBUG_CODE","배터리");
+
+            //공학용계산기
+
+            listImage.add(R.drawable.calculator1);
+            listImage.add(R.drawable.calculator2);
+            listImage.add(R.drawable.calculator3);
+        }
+
+        for (int i = 0; i < 3; i++) {
 
             ImageFragment imageFragment = new ImageFragment();
             Bundle bundle = new Bundle();
@@ -54,11 +98,9 @@ public class RentalActivity extends AppCompatActivity {
             imageFragment.setArguments(bundle);
             fragmentAdapter.addItem(imageFragment);
         }
-
         fragmentAdapter.notifyDataSetChanged();
-
-
     }
+
 
     void init() {
 
@@ -79,7 +121,7 @@ public class RentalActivity extends AppCompatActivity {
         rentalObject = intent.getStringExtra("Object");
 
 
-        if (rentalObject.equals(Const.notebook)) {
+        if (rentalObject.equals(notebook)) {
 
 
             //노트북
@@ -91,10 +133,19 @@ public class RentalActivity extends AppCompatActivity {
 
             //보조배터리
 
+            //노트북
+            textObject.setText(rentalObject);
+            Battery battery = new Battery();
+            textObjectInfo.setText(battery.modelInfo);
+
         } else if (rentalObject.equals(Const.calculator)) {
 
             //공학용계산기
 
+            //노트북
+            textObject.setText(rentalObject);
+            Calculator calculator = new Calculator();
+            textObjectInfo.setText(calculator.modelInfo);
 
         }
 
